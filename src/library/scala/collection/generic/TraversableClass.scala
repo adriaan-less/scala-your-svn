dd4c75d22861bfa924e7a5131f4660a7aeb17c55
Adriaan Moors <adriaanm@gmail.com>
2009-06-08 15:58:44 +0200

    encoding generalised constraints to implement unzip (as well as flatten/...)

--- src/library/scala/collection/generic/TraversableClass.scala
+++ src/library/scala/collection/generic/TraversableClass.scala
@@ -8,7 +8,7 @@
 // $Id: Traversable.scala 15188 2008-05-24 15:01:02Z stepancheg $
 package scala.collection.generic
 
-trait TraversableClass[+A, +CC[X] <: Traversable[X]] { 
+trait TraversableClass[+A, +CC[X] <: Traversable[X]] {
 
   /** The factory companion object that builds instances of class CC */
 
@@ -19,5 +19,19 @@ trait TraversableClass[+A, +CC[X] <: Traversable[X]] {
 
   /** The generic builder that builds instances of CC at arbitrary element types. */
   def genericBuilder[B]: Builder[B, CC[B]] = companion.newBuilder[B]
+  
+  // @M TODO: redundant... (but a self type like CC[A] does not type check)
+  def foreach[U](f: A => U): Unit 
+    
+  implicit def subtype_witness[T <: U, U](x: T): U = x  // @M TODO: move to Predef? (+ optimiser should eliminate it)
+  def unzip[X, Y](implicit c: A => (X, Y)): (CC[X], CC[Y]) = {
+    val b1 = genericBuilder[X]
+    val b2 = genericBuilder[Y]
+    foreach { case (x1: X, x2: Y) =>
+      b1 += x1
+      b2 += x2
+    }
+    (b1.result, b2.result)    
+  }
 }
 
