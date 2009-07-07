78f884a03dc6c0892328c9c8df2a09a2df44c64d
Adriaan Moors <adriaanm@gmail.com>
2009-07-07 11:54:06 +0200

    started work on fix for ticket 2101, but compiler bootstrap broke

--- src/compiler/scala/tools/nsc/symtab/Types.scala
+++ src/compiler/scala/tools/nsc/symtab/Types.scala
@@ -3705,10 +3705,18 @@ A type's typeSymbol should never be inspected directly.
          (res1 <:< res2) &&
          tp1.isInstanceOf[ImplicitMethodType] == tp2.isInstanceOf[ImplicitMethodType])
       case (PolyType(tparams1, res1), PolyType(tparams2, res2)) =>
-        (tparams1.length == tparams2.length &&
-         List.forall2(tparams1, tparams2) 
-           ((p1, p2) => p2.info.substSym(tparams2, tparams1) <:< p1.info) &&
-         res1 <:< res2.substSym(tparams2, tparams1))
+        (tparams1.length == tparams2.length && {
+          //@M for an example of why we need to generate fresh symbols,         see neg/tcpoly_ticket2101.scala
+          val tpsFresh = cloneSymbols(tparams1) // @M         cloneSymbols(tparams2) should be equivalent -- TODO: check
+          List.forall2(tparams1, tparams2)((p1, p2) =>
+            p2.info.substSym(tparams2, tpsFresh) <:< p1.info.substSym(tparams1, tpsFresh)) &&
+          res1.substSym(tparams1, tpsFresh) <:< res2.substSym(tparams2, tpsFresh)
+        })
+      // case (PolyType(tparams1, res1), PolyType(tparams2, res2)) =>
+      //   (tparams1.length == tparams2.length &&
+      //    List.forall2(tparams1, tparams2) 
+      //      ((p1, p2) => p2.info.substSym(tparams2, tparams1) <:< p1.info) &&
+      //    res1 <:< res2.substSym(tparams2, tparams1))
       case (TypeBounds(lo1, hi1), TypeBounds(lo2, hi2)) =>
         lo2 <:< lo1 && hi1 <:< hi2
       case (AnnotatedType(_,_,_), _) =>
