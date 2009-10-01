442dd5ac8fb5548d57f008cddd075f5e90cb79c5
Adriaan Moors <adriaanm@gmail.com>
2009-10-01 17:31:08 +0200

    comments in test file

--- test/files/pos/ticket2261.scala
+++ test/files/pos/ticket2261.scala
@@ -3,4 +3,7 @@ object Test {
   implicit def foo2bar[T](xs: List[T]): Bob[T] = new Bob[T]
   var x: Bob[Int] = null
   x = List(1,2,3)
+  // the problem here was that somehow the type variable that was used to infer the type argument for List.apply
+  // would accumulate several conflicting constraints
+  // can't find the right revision to reproduce the failure anymore...
 }
