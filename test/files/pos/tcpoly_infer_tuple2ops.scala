b82a263c7583b4e65fb112109da23ddbb342fe33
Adriaan Moors <adriaanm@gmail.com>
2009-10-22 17:31:43 +0200

    fixes to check files and removed nonapplicable test case

--- test/files/pos/tcpoly_infer_tuple2ops.scala
+++
@@ -1,4 +0,0 @@
-// test implementation of tuple2ops using generalized constraints, which relies on type constructor inference 
-object Test {
-  val x /*: List[Int] is inferred */ = (List(1,2,3), List(3,2,1)) map2 ((_: Int) + (_: Int))
-}
