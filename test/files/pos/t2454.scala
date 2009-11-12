81f76ed0ac9642c0c66967b3216566ec4be954f9
Adriaan Moors <adriaanm@gmail.com>
2009-11-12 16:27:29 +0100

    whitespace

--- test/files/pos/t2454.scala
+++ test/files/pos/t2454.scala
@@ -2,7 +2,7 @@ package am;
 
 trait One[M[_]] {
   val x : Int
-} 
+}
 
 trait Two[M[_,_]] {
   val x : Int
@@ -13,11 +13,11 @@ object Test {
   val x = new Two[Map] {
     val x = 5
   }
-  
+
   val o = new One[java.util.List] {
     val x = 1
   }
-  
+
   // Does not work
   val y = new Two[java.util.concurrent.ConcurrentHashMap] {
     val x = 3
