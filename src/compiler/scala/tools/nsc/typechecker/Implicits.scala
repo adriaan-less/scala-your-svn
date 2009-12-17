adriaan-less/scala-your-svn

commit e7f80f7eecaf84c92e5a294cd076c27cf47a89c5

Closed #2795. review by dubochet.
git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@20203 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/compiler/scala/tools/nsc/typechecker/Implicits.scala
@@ -725,13 +725,14 @@ self: Analyzer =>
       def manifestFactoryCall(constructor: String, tparg: Type, args: Tree*): Tree =
         if (args contains EmptyTree) EmptyTree
         else typedPos(tree.pos.focus) {
+          util.trace("manif fact ")(
           Apply(
             TypeApply(
               Select(gen.mkAttributedRef(if (full) FullManifestModule else PartialManifestModule), constructor),
               List(TypeTree(tparg))
             ),
             args.toList
-          )
+          ))
         }
       
       /** Creates a tree representing one of the singleton manifests.*/
@@ -770,10 +771,13 @@ self: Analyzer =>
                 EmptyTree // todo: change to existential parameter manifest
               else if (sym.isTypeParameterOrSkolem)
                 EmptyTree  // a manifest should have been found by normal searchImplicit
-              else
+              else {
+                val era = erasure.erasure(tp1)
+                val base = tp1.baseType(era.typeSymbol)
                 manifestFactoryCall(
                   "abstractType", tp,
-                  findSubManifest(pre) :: Literal(sym.name.toString) :: findManifest(tp1.bounds.hi) :: (args map findSubManifest): _*)
+                  findSubManifest(pre) :: Literal(sym.name.toString) :: gen.mkClassOf(base) :: (args map findSubManifest): _*)
+              }
             } else {
               EmptyTree  // a manifest should have been found by normal searchImplicit
             }

src/library/scala/reflect/ClassManifest.scala
@@ -207,9 +207,9 @@ object ClassManifest {
   /** ClassManifest for the abstract type `prefix # name'. `upperBound' is not
     * strictly necessary as it could be obtained by reflection. It was
     * added so that erasure can be calculated without reflection. */
-  def abstractType[T](prefix: OptManifest[_], name: String, upperBound: ClassManifest[_], args: OptManifest[_]*): ClassManifest[T] =
+  def abstractType[T](prefix: OptManifest[_], name: String, clazz: Predef.Class[_], args: OptManifest[_]*): ClassManifest[T] =
     new (ClassManifest[T] @serializable) {
-      def erasure = upperBound.erasure
+      def erasure = clazz
       override val typeArguments = args.toList
       override def toString = prefix.toString+"#"+name+argString
     }

src/library/scala/reflect/Manifest.scala
@@ -218,9 +218,9 @@ object Manifest {
   /** Manifest for the abstract type `prefix # name'. `upperBound' is not
     * strictly necessary as it could be obtained by reflection. It was
     * added so that erasure can be calculated without reflection. */
-  def abstractType[T](prefix: Manifest[_], name: String, upperBound: Manifest[_], args: Manifest[_]*): Manifest[T] =
+  def abstractType[T](prefix: Manifest[_], name: String, clazz: Predef.Class[_], args: Manifest[_]*): Manifest[T] =
     new (Manifest[T] @serializable) {
-      def erasure = upperBound.erasure
+      def erasure = clazz
       override val typeArguments = args.toList
       override def toString = prefix.toString+"#"+name+argString
     }

test/files/pos/t2795.scala
@@ -1,0 +1,16 @@
+package bug1
+
+trait Element[T] {
+}
+
+trait Config {
+  type T <: Element[T]
+  // XXX Following works fine:
+  // type T <: Element[_]
+}
+
+trait Transform { self: Config =>
+  def processBlock(block: Array[T]): Unit = {
+    var X = new Array[T](1)
+  }
+}

commit 0f286c035b543eb8c671ec13c4cce8df086a3a2f

Fixed build problem caused by r20203. Need to maintain old structure for bootstrap. review by dubochet.
git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@20208 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/compiler/scala/tools/nsc/typechecker/Implicits.scala
@@ -725,14 +725,13 @@ self: Analyzer =>
       def manifestFactoryCall(constructor: String, tparg: Type, args: Tree*): Tree =
         if (args contains EmptyTree) EmptyTree
         else typedPos(tree.pos.focus) {
-          util.trace("manif fact ")(
           Apply(
             TypeApply(
               Select(gen.mkAttributedRef(if (full) FullManifestModule else PartialManifestModule), constructor),
               List(TypeTree(tparg))
             ),
             args.toList
-          ))
+          )
         }
       
       /** Creates a tree representing one of the singleton manifests.*/

src/library/scala/reflect/ClassManifest.scala
@@ -214,6 +214,18 @@ object ClassManifest {
       override def toString = prefix.toString+"#"+name+argString
     }
 
+  /** ClassManifest for the abstract type `prefix # name'. `upperBound' is not
+    * strictly necessary as it could be obtained by reflection. It was
+    * added so that erasure can be calculated without reflection.
+    * todo: remove after next boostrap
+    */
+  def abstractType[T](prefix: OptManifest[_], name: String, upperbound: ClassManifest[_], args: OptManifest[_]*): ClassManifest[T] =
+    new (ClassManifest[T] @serializable) {
+      def erasure = upperbound.erasure
+      override val typeArguments = args.toList
+      override def toString = prefix.toString+"#"+name+argString
+    }
+
   /** ClassManifest for the intersection type `parents_0 with ... with parents_n'. */
   def intersectionType[T](parents: ClassManifest[_]*): ClassManifest[T] =
     new (ClassManifest[T] @serializable) {

src/library/scala/reflect/Manifest.scala
@@ -225,6 +225,18 @@ object Manifest {
       override def toString = prefix.toString+"#"+name+argString
     }
 
+  /** Manifest for the abstract type `prefix # name'. `upperBound' is not
+    * strictly necessary as it could be obtained by reflection. It was
+    * added so that erasure can be calculated without reflection.
+    * todo: remove after next bootstrap
+    */
+  def abstractType[T](prefix: Manifest[_], name: String, upperbound: ClassManifest[_], args: Manifest[_]*): Manifest[T] =
+    new (Manifest[T] @serializable) {
+      def erasure = upperbound.erasure
+      override val typeArguments = args.toList
+      override def toString = prefix.toString+"#"+name+argString
+    }
+
   /** Manifest for the intersection type `parents_0 with ... with parents_n'. */
   def intersectionType[T](parents: Manifest[_]*): Manifest[T] =
     new (Manifest[T] @serializable) {