20a63bff1ca29d220b2261a34e45b2704665ee37
Adriaan Moors <adriaanm@gmail.com>
2010-02-01 12:33:44 +0100

    temporary bigger test case

--- test/files/pos/t2936.scala
+++ test/files/pos/t2936.scala
@@ -9,3 +9,11 @@ class X {
   def disposes[T <: Disposable](disposable: T) { }
   def disposes(datagramSocket: DatagramSocket): DatagramSocket = datagramSocket
 }
+
+// for easy testing, this must also still work:
+trait Monad[T <: Bound[T], MyType[x <: Bound[x]], Bound[_]] {
+  def map[S <: Bound[S]](f: T => S): MyType[S]  // etc
+}                                                                               
+class Set[T <: Ordered[T]] extends Monad[T, Set, Ordered] {
+  def map[S <: Ordered[S]](f: T => S): Set[S] = error("TODO") // etc
+}                                                                   
