6fe620ef8fd080374b0dd26f73074c8ddf664514
Adriaan Moors <adriaanm@gmail.com>
2010-06-22 23:07:19 +0200

    test files

---
+++ test/files/pos/depmet_infer_single_ret.scala
@@ -0,0 +1,15 @@
+object Test{
+  def ?[S <: AnyRef](implicit w : S) : w.type = w
+  
+  implicit def ZeroZipWith[S] = new ZipWith[S] {
+    type T = Stream[S]
+  }    
+
+  // bug: return type should be inferred instead of "error: recursive method apply needs result type"
+  def apply[S: ZipWith](s : S) = ?[ZipWith[S]].zipWith(s)
+  
+  trait ZipWith[S] {
+    type T
+    def zipWith : S => T
+  }
+}  
