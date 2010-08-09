c3a844ca4e8c3c855b4aa4a7132117f27e2c0922
Adriaan Moors <adriaanm@gmail.com>
2010-08-09 12:32:51 +0200

    closes #3582: typedTypeDef needs to run in a new typer for TypeDefs with type parameters
    this was honored when typedTypeDef was called by typed1, but other callers did not
    this would cause higher-order type parameters to be re-entered in the scope of a method or a class
    
    TODO: should we recycle scopes for higher-order type params?

--- test/files/pos/t3582b.scala
+++ test/files/pos/t3582b.scala
@@ -1,3 +1,6 @@
-object Test {
-  def primeBuffer[T, G[T]]: G[T] =  null.asInstanceOf[G[T]]
+object ParamScoping {
+  // how come T is properly scoped in the return type, but not in the body of the method
+  def noOverlapFOwithHO[T, G[T]]: G[T] =  null.asInstanceOf[G[T]] 
+  // def canAccessLaterFOinHO[A, B[x <: C], C]: G[T] =  null.asInstanceOf[G[T]]
+  // def canAccessEarlierFOinHO[A, B[x <: A], C]: G[T] =  null.asInstanceOf[G[T]]  
 }
