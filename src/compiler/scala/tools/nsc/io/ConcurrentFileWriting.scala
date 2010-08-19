671a70dc61e0be4d323874f3d10a185d2e6d70ee
Adriaan Moors <adriaanm@gmail.com>
2010-08-19 12:09:41 +0200

    rethrow exception in thread so the thread dies -- not sure what to do about multiple failures

--- src/compiler/scala/tools/nsc/io/ConcurrentFileWriting.scala
+++ src/compiler/scala/tools/nsc/io/ConcurrentFileWriting.scala
@@ -8,16 +8,37 @@ trait ConcurrentFileWriting {
   val global: Global
   import global.{settings, informProgress, error}
 
-  private val cmdQ = new LinkedBlockingQueue[() => Unit]()
-  private def drainCmdQ() = {
-    import scala.collection.JavaConversions._
-    val cmds = new java.util.LinkedList[() => Unit]()
-    cmdQ drainTo cmds
-    for(cmd <- cmds) cmd()
+  /** Performs `writer` on the output stream of `file`, and calls `informProgress` when that's done.
+   *
+   * Depending on the number of available processors, `writer` may be executed concurrently.
+   * However, `informProgress` will run in the thread that calls `scheduleWrite` or `waitForWriters`.
+   * Similarly, an exception that was caused while writing will be propagated to the calling thread.
+   */
+  def scheduleWrite(file: AbstractFile, msg: String = "wrote ")(writer: DataOutputStream => Unit) = 
+    if(nThreads > 1) {
+      writerPool.execute(new Writer(file, writer, msg))
+      drainCmdQ()
+    } else { // non-concurrent
+      val outstream = new DataOutputStream(file.output)
+      writer(outstream)
+      outstream.close()
+      informProgress(msg + file)
+    }
+
+  /** Blocks until all writers are done.
+   * 
+   * May perform pending informProgress's and throw the first of the exceptions that occurred concurrently.
+   */
+  def waitForWriters() = {
+    writerPool.shutdown()
+    if(!writerPool.awaitTermination(300, java.util.concurrent.TimeUnit.SECONDS))
+      error("Writers did not finish in under 5 minutes after bytecode was generated!")
+    drainCmdQ()
   }
 
   private val nThreads = Runtime.getRuntime().availableProcessors()
   private val writerPool = Executors.newFixedThreadPool(nThreads)
+  private val cmdQ = new LinkedBlockingQueue[() => Unit]()
 
   private class Writer(val file: AbstractFile, val writer: DataOutputStream => Unit, val msg: String) extends Runnable {
     def run(): Unit = {
@@ -28,39 +49,21 @@ trait ConcurrentFileWriting {
         val progMsg: String = msg + file
         cmdQ put {() => informProgress(progMsg)}
       } catch {
-        case t: Exception => 
-          if(settings.debug.value) t.printStackTrace()
-          cmdQ put {() => throw t}
+        case ex: Exception => 
+          if(settings.debug.value) ex.printStackTrace()
+          cmdQ put {() => throw ex}
+          throw ex
       }
     }
   }
-
-  /** Performs `writer` on the output stream of `file`, and calls `informProgress` when that's done.
-   *
-   * Depending on the number of available processors, `writer` may be executed concurrently.
-   * However, `informProgress` will run in the thread that calls `scheduleWrite` or `waitForWriters`.
-   * Similarly, any exceptions caused while writing will be propagated to the calling thread.
-   */
-  def scheduleWrite(file: AbstractFile, msg: String = "wrote ")(writer: DataOutputStream => Unit) = 
-    if(nThreads > 1) {
-      writerPool.execute(new Writer(file, writer, msg))
-      drainCmdQ()
-    }
-    else { // non-concurrent
-      val outstream = new DataOutputStream(file.output)
-      writer(outstream)
-      outstream.close()
-      informProgress(msg + file)
-    }
-
-  /** Blocks until all writers are done.
-   * 
-   * May perform pending informProgress's and throw exceptions that occurred concurrently.
-   */
-  def waitForWriters() = {
-    writerPool.shutdown()
-    if(!writerPool.awaitTermination(300, java.util.concurrent.TimeUnit.SECONDS))
-      error("Writers did not finish in under 5 minutes after bytecode was generated!")
-    drainCmdQ()
+  
+  // executes commands that arise in worker threads in main thread (informProgress/throwing an exception)
+  // TODO: when multiple exceptions re-throws are put in the queue, the first one will cause drainCmdQ to abort
+  //       should we aggresgate all exceptions into a single one instead?
+  private def drainCmdQ() = {
+    import scala.collection.JavaConversions._
+    val cmds = new java.util.LinkedList[() => Unit]()
+    cmdQ drainTo cmds
+    for(cmd <- cmds) cmd()
   }
 }
