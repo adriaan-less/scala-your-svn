85b417b82d8e0b62e459e847184ca4ac33671151
Adriaan Moors <adriaanm@gmail.com>
2010-09-15 11:55:19 +0200

    renaming and removed failing tests -- these should all pass

--- test/files/pos/depmet_implicit_norm_ret.scala
+++ test/files/pos/depmet_implicit_norm_ret.scala
@@ -23,7 +23,7 @@ object Test{
   }
   
   // bug: inferred return type = (Stream[A]) => java.lang.Object with Test.ZipWith[B]{type T = Stream[B]}#T
-  // this seems incompatible with vvvvvvvvvvvvvvvvvvvvvv   -- missing case in normalize?
+  // this seems incompatible with vvvvvvvvvvvvvvvvvvvvvv   -- #3731
   def map[A,B](f : A => B)   /* : Stream[A] => Stream[B]*/ = ZipWith(f) 
   val tst: Stream[Int] = map{x: String => x.length}(Stream("a"))  
 }  
