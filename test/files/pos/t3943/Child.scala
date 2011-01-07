b861db6d258d6dcd2e26c403e2e4261c9de109f7
Adriaan Moors <adriaanm@gmail.com>
2011-01-07 15:27:00 +0100

    test runnable

--- test/files/pos/t3943/Child.scala
+++ test/files/pos/t3943/Child.scala
@@ -1,7 +1,10 @@
-package test
-
 // error: class Sub needs to be abstract, since method f in class Child of type 
 // (x$1: test.TopLevelParent[T]#Inner)Unit is not defined
 class Sub[A] extends Child[A] {
-  def f(ic: Child[A]#Inner): Unit = ()
+  def f(ic: Child[A]#Inner): Unit = println("peekaboo")
+}
+
+object Test extends Application {
+  val s = new Sub[Int]
+  s.f(new s.Inner)
 }
