adriaan-less/scala-your-svn

commit f52a5fe2bd2a006772df67f917739ae803e261c3

Separated abstract and concrete value members in scaladoc. This looks like such a runaway improvement to me that I'm just checking it in, but if anyone feels differently say the word. I eliminated the abstract/concrete filter entirely as it is extraneous once you can just see which is which.
Closes #3831, review by malayeri.

git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@24658 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/compiler/scala/tools/nsc/doc/html/page/Template.scala
@@ -30,6 +30,9 @@ class Template(tpl: DocTemplateEntity) extends HtmlPage {
   
   val valueMembers =
     tpl.methods ++ tpl.values ++ tpl.templates.filter(x => x.isObject || x.isPackage) sorted
+
+  val (absValueMembers, concValueMembers) =
+    valueMembers partition (_.isAbstract)
   
   val typeMembers =
     tpl.abstractTypes ++ tpl.aliasTypes ++ tpl.templates.filter(x => x.isTrait || x.isClass) sorted
@@ -82,12 +85,6 @@ class Template(tpl: DocTemplateEntity) extends HtmlPage {
               <ol><li class="public in">Public</li><li class="all out">All</li></ol>
             </div>
           }
-          {
-            <div id="impl">
-              <span class="filtertype">Impl.</span>
-              <ol><li class="concrete in">Concrete</li><li class="abstract in">Abstract</li></ol>
-            </div>
-          }
         </div>
 
         { if (constructors.isEmpty) NodeSeq.Empty else
@@ -103,11 +100,18 @@ class Template(tpl: DocTemplateEntity) extends HtmlPage {
               <ol>{ typeMembers map (memberToHtml(_)) }</ol>
             </div>
         }
+        
+        { if (absValueMembers.isEmpty) NodeSeq.Empty else
+            <div id="abstract values" class="values members">
+              <h3>Abstract Value Members</h3>
+              <ol>{ absValueMembers map (memberToHtml(_)) }</ol>
+            </div>
+        }
 
-        { if (valueMembers.isEmpty) NodeSeq.Empty else
-            <div id="values" class="values members">
-              <h3>Value Members</h3>
-              <ol>{ valueMembers map (memberToHtml(_)) }</ol>
+        { if (concValueMembers.isEmpty) NodeSeq.Empty else
+            <div id="concrete values" class="values members">
+              <h3>{ if (absValueMembers.isEmpty) "Value Members" else "Concrete Value Members" }</h3>
+              <ol>{ concValueMembers map (memberToHtml(_)) }</ol>
             </div>
         }
 

src/compiler/scala/tools/nsc/doc/html/resource/lib/template.js
@@ -69,24 +69,6 @@ $(document).ready(function(){
             filter();
         };
     });
-    $("#impl > ol > li.concrete").click(function() {
-        if ($(this).hasClass("out")) {
-            $(this).removeClass("out").addClass("in");            
-            $("li[data-isabs='false']").show();
-        } else {
-            $(this).removeClass("in").addClass("out");
-            $("li[data-isabs='false']").hide();
-        }
-    });
-    $("#impl > ol > li.abstract").click(function() {
-        if ($(this).hasClass("out")) {
-            $(this).removeClass("out").addClass("in");                        
-            $("li[data-isabs='true']").show();
-        } else {
-            $(this).removeClass("in").addClass("out");
-            $("li[data-isabs='true']").hide();
-        }
-    });
     $("#order > ol > li.alpha").click(function() {
         if ($(this).hasClass("out")) {
             $(this).removeClass("out").addClass("in");

commit 277232637810118c8f5615c9a45c874cd08a95fc

[scaladoc] Change div id's of abstract and concrete members so that "order by inheritance" action works properly. Fixes issue introduced in r24658. Review by extempore.
git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@24678 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/compiler/scala/tools/nsc/doc/html/page/Template.scala
@@ -102,14 +102,14 @@ class Template(tpl: DocTemplateEntity) extends HtmlPage {
         }
         
         { if (absValueMembers.isEmpty) NodeSeq.Empty else
-            <div id="abstract values" class="values members">
+            <div id="values" class="values members">
               <h3>Abstract Value Members</h3>
               <ol>{ absValueMembers map (memberToHtml(_)) }</ol>
             </div>
         }
 
         { if (concValueMembers.isEmpty) NodeSeq.Empty else
-            <div id="concrete values" class="values members">
+            <div id="values" class="values members">
               <h3>{ if (absValueMembers.isEmpty) "Value Members" else "Concrete Value Members" }</h3>
               <ol>{ concValueMembers map (memberToHtml(_)) }</ol>
             </div>