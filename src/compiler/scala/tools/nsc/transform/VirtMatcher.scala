80664eab6c18667c898467eedfb279157f44eaec
Adriaan Moors <adriaanm@gmail.com>
2011-07-29 12:11:35 +0200

    restructured a bit, documentation, most of the translation structure seems ok -- now need to interpret pattern trees ccorrectly and turn them into extractor-style patterns

--- src/compiler/scala/tools/nsc/transform/VirtMatcher.scala
+++ src/compiler/scala/tools/nsc/transform/VirtMatcher.scala
@@ -10,6 +10,19 @@ import symtab._
 import Flags.{ CASE => _, _ }
 import scala.collection.mutable.ListBuffer
 
+/** Translate pattern matching into method calls. Turn all patterns into unapplies
+
+
+    scrut match { case Person(father@Person(_, fatherName), name) if fatherName == name => }
+    scrut match { case Person(father, name) => father match {case Person(_, fatherName) => }}
+    Person.unapply(scrut) >> ((father, name) => (Person.unapply(father) >> (_, fatherName) => check(fatherName == name) >> (_ => body)))
+
+    (a => (Person.unapply(a).>>(
+      b => Person.unapply(b._1).>>(
+        c => check(c._2 == b._2).>>(
+          d => body)))))(scrut)
+
+  */
 trait VirtMatcher extends ast.TreeDSL {
   import global._
   import definitions._
@@ -28,15 +41,119 @@ trait VirtMatcher extends ast.TreeDSL {
     'foo'   --> inside ` ... `, 'foo' interpolates the result of evaluating foo as code
 
     */
-    import CODE._
+    type TreeXForm = Tree => Tree
+    type TreeMaker = (Tree, TreeXForm => (TreeXForm, TreeXForm))
+
+    /** Implement a pattern match by turning its cases (including the implicit failure case) 
+      * into the corresponding the (monadic) extractors and combining them with the `orElse` combinator.
+      */
+    def X(tree: Tree): Tree = tree match {
+      case Match(scrut, cases) => 
+        val scrutSym = freshSym(currentOwner, tree.pos) setInfo scrut.tpe
+        val xTree = // ( x => Xcase1.orElse(Xcase2).....orElse(fail) )(scrut)
+          mkApply(mkFunTree(scrutSym,
+            (cases :\ mkFailTree)((currCase, accumCases) => 
+              mkOrElse(Xcase(scrutSym, currCase), accumCases))),
+          scrut)
+        println("xformed: "+ xTree)
+        localTyper typed xTree
+      case t => t
+    }
+
+    def Xcase(scrutSym: Symbol, tree: Tree): Tree = {
+      // (o => (o(foo), newO)) :: (o => (o(foo), newO')) :: (o => (o(foo), newO'')) :: (o => (o(foo), newO'''))
+      // (identity(foo), newO) :: (newO(foo), newO') :: (newO'(foo), newO'') :: (newO''(foo), newO''')
+      def threadSubstitution(substTreeMakers: List[TreeMaker]): List[(Tree, TreeXForm)] = 
+        substTreeMakers.foldLeft((List[(Tree, TreeXForm)](), identity[Tree](_))){ case ((accumTreeMakers, accumSubst), (extractorTree, substTreeMaker)) => 
+          val (nestedTreeMaker, newSubst) = substTreeMaker(accumSubst)
+          ((extractorTree, nestedTreeMaker) :: accumTreeMakers, newSubst)
+        }._1.reverse
+
+      tree match {
+        case CaseDef(pattern, guard, body) => 
+          threadSubstitution(Xpat(scrutSym)(pattern) ++ Xguard(guard)).foldRight(mkSuccessTree(X(body))){
+            case ((extractor, mkFunAndSubst), tree) => mkBindTree(extractor, mkFunAndSubst(tree))
+          }
+      }
+    }
+
+    def mkGuardTree(guard: Tree): Tree = mkCheckTree(X(guard))
+    def Xguard(guard: Tree): List[TreeMaker] = 
+      if (guard == EmptyTree) List()
+      else List(
+        (mkGuardTree(X(guard)), 
+          { outerSubst => 
+            (nestedTree => mkFunTree(freshSym(currentOwner, nestedTree.pos) setInfo UnitClass.tpe, 
+             outerSubst(nestedTree)), outerSubst) // guard does not bind any variables, so next subst is the current one
+          }))
+
+    def Xpat(scrutSym: Symbol)(pattern: Tree): List[TreeMaker] = {
+      /** `patTree` is the extractor call
+        * `patBinders` are the variables bound by this pattern --> must become tuple selections on extractor's result
+        */
+      def mkPatTreeMaker(patTree: Tree, patBinders: List[Symbol]): TreeMaker = 
+        (patTree, 
+          { outerSubst: TreeXForm => 
+              println("patbinderstpe "+ (patBinders map (_.tpe)))
+              val binder = freshSym(currentOwner, patTree.pos) setInfo tupleTypeOrUnit(patBinders map (_.tpe))
+              def nextSubst(tree: Tree): Tree = new TreeSubstituter(patBinders, (1 to patBinders.length).toList map mkPatBinderTupleSel(binder)).transform(outerSubst(tree))
+              ((nestedTree: Tree) => mkFunTree(binder, 
+               nextSubst(nestedTree)), nextSubst)
+          })
+
+      val res = new ListBuffer[TreeMaker]
+      def traverseDepthFirst(currSym: Symbol, t: Tree): Unit = {
+          val (nestedBinders, nestedTrees) = nestedBindersAndTrees(t)
+          res += mkPatTreeMaker(mkApplyUnapply(toUnapply(t), currSym), nestedBinders)  // TODO scrutSym.owner, 
+          (nestedBinders, nestedTrees).zipped foreach traverseDepthFirst
+      }
+      traverseDepthFirst(scrutSym, pattern)
+      res.toList
+    }
+
+
+// tree exegesis, rephrasing everything in terms of extractors 
+    /** A conservative approximation of which patterns do not discern anything.
+      */
+    def isWildcardPattern(pat: Tree): Boolean = pat match {
+      case Bind(_, body)        => isWildcardPattern(body)
+      case Ident(nme.WILDCARD)  => true
+      case x: Ident             => treeInfo.isVarPattern(x)
+      case Alternative(ps)      => ps forall isWildcardPattern
+      case EmptyTree            => true
+      case _                    => false
+    }
+
+    /** Decompose the pattern in `tree`, returning the symbols it binds at the top-level, and its pattern sub-trees (if any)
+      */
+    def nestedBindersAndTrees(tree: Tree): (List[Symbol], List[Tree]) = tree match {
+      case Apply(fun, args) => (args map (_.symbol), args filterNot isWildcardPattern)
+      case UnApply(unfun, args) => 
+        val binderTypes = analyzer.unapplyTypeList(unfun.symbol, unfun.tpe)
+        println("unapp: "+ (unfun, args, args map (_.symbol), binderTypes zip (args map (_.symbol.info))))
+        (args map (_.symbol), args filterNot isWildcardPattern)
+      case t => (Nil, Nil)
+    }
+
+    def toUnapply(tree: Tree): Tree = tree
+
+// code gen
+    var ctr = 0
+    def freshSym(owner: Symbol, pos: Position) = {ctr += 1; assert(owner ne null); assert(owner ne NoSymbol); new TermSymbol(owner, pos, ("x"+ctr).toTermName)}
+
     def matcherTycon: Type = OptionClass.typeConstructor // TODO: determined by solving M in `implicitly[MatchingStrategy[M]]`
     def tupleTypeOrUnit(tps: List[Type]): Type = if(tps.isEmpty) UnitClass.tpe else tupleType(tps)
     def matchingStrategyTycon = getMember(PredefModule, "MatchingStrategy".toTypeName)
     def Predef_implicitly = getMember(PredefModule, "implicitly".toTermName)
+
+    import CODE._
+    // TODO: lift out this call and cache the result
     def mkImplicitMatcherTree: Tree = TypeApply(REF(Predef_implicitly), List(TypeTree(appliedType(matchingStrategyTycon.typeConstructor, List(matcherTycon)))))  // implicitly[MatchingStrategy[matcherTycon]].fail
     def mkFailTree: Tree = mkImplicitMatcherTree DOT "fail".toTermName 
-    def mkApplyCaseOrElse(thisCase: Tree, arg: Tree, elseCase: Tree): Tree = ((thisCase APPLY arg) DOT "orElse".toTermName) APPLY elseCase
-    def mkBindTree(a: Tree, b: Tree): Tree = (a DOT ">>".toTermName) APPLY b
+    def mkSuccessTree(res: Tree): Tree = (mkImplicitMatcherTree DOT "success".toTermName) APPLY res
+    def mkOrElse(thisCase: Tree, elseCase: Tree): Tree = (thisCase DOT "orElse".toTermName) APPLY elseCase
+    def mkApply(fun: Tree, arg: Tree): Tree = fun APPLY arg
+    def mkBindTree(a: Tree, b: Tree): Tree = (a DOT "flatMap".toTermName) APPLY b
     def mkFunTree(arg: Symbol, body: Tree): Tree = Function(List(ValDef(arg)), body)
     def mkPatBinderTupleSel(binder: Symbol)(i: Int): Tree = REF(binder) DOT ("_"+i).toTermName // make tree that accesses the i'th component of the tuple referenced by binder
     def mkCheckTree(t: Tree): Tree = (mkImplicitMatcherTree DOT "check".toTermName) APPLY t
@@ -45,85 +162,5 @@ trait VirtMatcher extends ast.TreeDSL {
         case UnApply(t, _) => REF(treeInfo.methPart(t).symbol)
         case t => t
       }) APPLY REF(arg)
-
-    def X(tree: Tree): Tree = tree match {
-      case Match(scrut, cases) => 
-        val xTree = cases.foldRight(mkFailTree)((currCase, accumCases) => mkApplyCaseOrElse(Xcase(scrut.tpe, currCase), scrut, accumCases))
-        println("xformed: "+ xTree)
-        localTyper typed xTree
-      case t => t
-    }
-
-    def Xcase(scrutTpe: Type, tree: Tree): Tree = tree match {
-      case CaseDef(pattern, guard, body) => 
-        val scrutSym = freshSym(currentOwner, tree.pos) setInfo scrutTpe
-        val extractor = threadSubstitution(Xpat(scrutSym)(pattern) ++ Xguard(guard)).foldRight(X(body)){
-          case ((extractor, mkFunAndSubst), tree) => mkBindTree(extractor, mkFunAndSubst(tree))
-        }
-
-        mkFunTree(scrutSym, extractor)
-    }
-
-    var ctr = 0
-    def freshSym(owner: Symbol, pos: Position) = {ctr += 1; assert(owner ne null); assert(owner ne NoSymbol); new TermSymbol(owner, pos, ("x"+ctr).toTermName)}
-
-    type TreeXForm = Tree => Tree
-    type TreeMaker = (Tree, TreeXForm => (TreeXForm, TreeXForm))
-
-    // (o => (o(foo), newO)) :: (o => (o(foo), newO')) :: (o => (o(foo), newO'')) :: (o => (o(foo), newO'''))
-    // (identity(foo), newO) :: (newO(foo), newO') :: (newO'(foo), newO'') :: (newO''(foo), newO''')
-    def threadSubstitution(substTreeMakers: List[TreeMaker]): List[(Tree, TreeXForm)] = 
-      substTreeMakers.foldLeft((List[(Tree, TreeXForm)](), identity[Tree](_))){ case ((accumTreeMakers, accumSubst), (extractorTree, substTreeMaker)) => 
-        val (nestedTreeMaker, newSubst) = substTreeMaker(accumSubst)
-        ((extractorTree, nestedTreeMaker) :: accumTreeMakers, newSubst)
-      }._1.reverse
-
-    def mkGuardTree(guard: Tree): Tree = mkCheckTree(X(guard))
-    def Xguard(guard: Tree): List[TreeMaker] = 
-      if (guard == EmptyTree) List()
-      else List((mkGuardTree(X(guard)), { outerSubst => (nestedTree => mkFunTree(freshSym(currentOwner, nestedTree.pos) setInfo UnitClass.tpe, outerSubst(nestedTree)), outerSubst) })) // guard does not bind any variables, 
-
-    def treeSubst(from: List[Symbol], to: List[Tree])(tree: Tree): Tree = new TreeSubstituter(from, to).transform(tree)
-
-    def mkPatTreeMaker(patTree: Tree, patBinders: List[Symbol]): TreeMaker = 
-      (patTree, { outerSubst: TreeXForm => 
-                    println("patbinderstpe "+ (patBinders map (_.tpe)))
-                    val binder = freshSym(currentOwner, patTree.pos) setInfo tupleTypeOrUnit(patBinders map (_.tpe))
-                    def nextSubst(tree: Tree): Tree = treeSubst(patBinders, (1 to patBinders.length).toList map mkPatBinderTupleSel(binder))(outerSubst(tree))
-                    ((nestedTree: Tree) => mkFunTree(binder, nextSubst(nestedTree)), nextSubst)
-                })
-
-    def Xpat(scrut: Symbol)(pattern: Tree): List[TreeMaker] = {
-      depthFirstFlatten(scrut)(pattern) map {case (t, bs) => mkPatTreeMaker(t, bs)} // TODO scrut.owner, 
-    }
-
-    def depthFirstFlatten(rootSym: Symbol)(pattern: Tree): List[(Tree /* extractor call */, List[Symbol] /* the variables bound by this pattern --> must become tuple selections on extractor's result*/)] = {
-      val res = new ListBuffer[(Tree, List[Symbol])]
-      def traverse(currSym: Symbol, t: Tree): Unit = {
-          val (nestedBinders, nestedTrees) = nestedBindersAndTrees(t)
-          res += Tuple2(mkApplyUnapply(toUnapply(t), currSym), nestedBinders)
-          (nestedBinders, nestedTrees).zipped foreach traverse
-      }
-      traverse(rootSym, pattern)
-      res.toList
-    }
-
-    def nestedBindersAndTrees(tree: Tree): (List[Symbol], List[Tree]) = tree match {
-      case Apply(fun, args) => (args map (_.symbol), args filterNot (_.isInstanceOf[Bind]))
-      case t => (Nil, Nil)
-    }
-
-    def toUnapply(tree: Tree): Tree = tree
-
-    /*
-    scrut match { case Person(father@Person(_, fatherName), name) if fatherName == name => }
-    scrut match { case Person(father, name) => father match {case Person(_, fatherName) => }}
-    Person.unapply(scrut) >> ((father, name) => (Person.unapply(father) >> (_, fatherName) => check(fatherName == name) >> (_ => body)))
-
-    a => (Person.unapply(a).>>(
-      b => Person.unapply(b._1).>>(
-        c => check(c._2 == b._2).>>(
-          d => body))))
-    */
   }
 }
